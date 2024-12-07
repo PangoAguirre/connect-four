a00ce604878fb60cf515a1284762adc985dc9300
PangoAguirre <bcamilo.aguirre@udea.edu.co>
2024-12-06 19:00:20 -0500

    bug fix

--- src/logic/board.js
+++ src/logic/board.js
@@ -18,12 +18,22 @@ export const checkWinner = (index, board, turn) => {
     const directions = [1, -1, 7, -7, 8, -8, 6, -6];
 
     for (let dir of directions) {
-        if (
-            board[index + dir] === turn &&
-            board[index + dir * 2] === turn &&
-            board[index + dir * 3] === turn
-        ) {
-            return turn;
+        if (!((index + 1) % 7 === 0)) {
+            if (
+                board[index + dir] === turn &&
+                board[index + dir * 2] === turn &&
+                board[index + dir * 3] === turn
+            ) {
+                return turn;
+            }
+        } else if (!(dir === 1) && (index + 1) % 7 === 0) {
+            if (
+                board[index + dir] === turn &&
+                board[index + dir * 2] === turn &&
+                board[index + dir * 3] === turn
+            ) {
+                return turn;
+            }
         }
     }
 
